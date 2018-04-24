797192cf222af3606293e43a1651afe746b0da2c
littlehell <hutianxing1992@live.com>
2018-04-24 16:21:23 +0800

    test 0.0.2

---
+++ cutpic02.py
@@ -0,0 +1,14 @@
+import os
+
+os.system('convert E_CO_00z_d02.png -crop 480x670+280+180 nE_CO_00z_d02.png')
+os.system('convert E_NO_00z_d02.png -crop 480x670+280+180 nE_NO_00z_d02.png')
+os.system('convert E_CO_12z_d02.png -crop 480x670+280+180 nE_CO_12z_d02.png')
+os.system('convert E_NO_12z_d02.png -crop 480x670+280+180 nE_NO_12z_d02.png')
+os.system('convert E_PM25I_00z_d02.png -crop 480x670+280+180 nE_PM25I_00z_d02.png')
+os.system('convert E_PM25I_12z_d02.png -crop 480x670+280+180 nE_PM25I_12z_d02.png')
+os.system('convert E_PM25J_00z_d02.png -crop 480x670+280+180 nE_PM25J_00z_d02.png')
+os.system('convert E_PM25J_12z_d02.png -crop 480x670+280+180 nE_PM25J_12z_d02.png')
+os.system('convert E_PM_10_00z_d02.png -crop 480x670+280+180 nE_PM10_00z_d02.png')
+os.system('convert E_PM_10_12z_d02.png -crop 480x670+280+180 nE_PM10_12z_d02.png')
+os.system('convert E_SO2_00z_d02.png -crop 480x670+280+180 nE_SO2_00z_d02.png')
+os.system('convert E_SO2_12z_d02.png -crop 480x670+280+180 nE_SO2_12z_d02.png')
